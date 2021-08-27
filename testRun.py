d843fc71dc467fe2fd3e78e72edd711c16e84160
TroyLong <tlong1299@gmail.com>
2021-08-26 23:40:14 -0500

    This is a more developed example from the scikit documentation, with an alteration to import an image

--- testRun.py
+++ testRun.py
@@ -1,41 +1,52 @@
-#%%
-import numpy as np
-import matplotlib.pyplot as plt
 from scipy import ndimage as ndi
+import matplotlib.pyplot as plt
 
+from skimage.morphology import disk
 from skimage.segmentation import watershed
-from skimage.feature import peak_local_max
+from skimage.segmentation import mark_boundaries
+from skimage import data
+from skimage.filters import rank
+from skimage.util import img_as_ubyte
+from skimage.io import imread
 
 
-# Generate an initial image with two overlapping circles
-x, y = np.indices((80, 80))
-x1, y1, x2, y2 = 28, 28, 44, 52
-r1, r2 = 16, 20
-mask_circle1 = (x - x1)**2 + (y - y1)**2 < r1**2
-mask_circle2 = (x - x2)**2 + (y - y2)**2 < r2**2
-image = np.logical_or(mask_circle1, mask_circle2)
+image = img_as_ubyte(imread("./twocircles.png",as_gray=True))
 
-# Now we want to separate the two objects in image
-# Generate the markers as local maxima of the distance to the background
-distance = ndi.distance_transform_edt(image)
-coords = peak_local_max(distance, footprint=np.ones((3, 3)), labels=image)
-mask = np.zeros(distance.shape, dtype=bool)
-mask[tuple(coords.T)] = True
-markers, _ = ndi.label(mask)
-labels = watershed(-distance, markers, mask=image)
+# denoise image
+denoised = rank.median(image, disk(2))
 
-fig, axes = plt.subplots(ncols=3, figsize=(9, 3), sharex=True, sharey=True)
+# find continuous region (low gradient -
+# where less than 10 for this image) --> markers
+# disk(5) is used here to get a more smooth image
+markers = rank.gradient(denoised, disk(5)) < 10
+markers = ndi.label(markers)[0]
+
+# local gradient (disk(2) is used to keep edges thin)
+gradient = rank.gradient(denoised, disk(2))
+
+# process the watershed
+labels = watershed(gradient, markers)
+
+# display results
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(8, 8),
+                         sharex=True, sharey=True)
 ax = axes.ravel()
 
 ax[0].imshow(image, cmap=plt.cm.gray)
-ax[0].set_title('Overlapping objects')
-ax[1].imshow(-distance, cmap=plt.cm.gray)
-ax[1].set_title('Distances')
-ax[2].imshow(labels, cmap=plt.cm.nipy_spectral)
-ax[2].set_title('Separated objects')
+ax[0].set_title("Original")
+
+ax[1].imshow(gradient, cmap=plt.cm.nipy_spectral)
+ax[1].set_title("Local Gradient")
+
+ax[2].imshow(markers, cmap=plt.cm.nipy_spectral)
+ax[2].set_title("Markers")
+
+ax[3].imshow(image, cmap=plt.cm.gray)
+ax[3].imshow(mark_boundaries(image, labels))
+ax[3].set_title("Segmented")
 
 for a in ax:
-    a.set_axis_off()
+    a.axis('off')
 
 fig.tight_layout()
 plt.show()
