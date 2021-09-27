0ee3d4c8d839ff349e0097cff6593403536b51a3
TroyLong <tlong1299@gmail.com>
2021-09-27 09:58:45 -0500

    created general.py and trimmed tree size.

---
+++ general.py
@@ -0,0 +1,42 @@
+import os
+import cv2 as cv
+import segmentImage as sI
+import matplotlib.pyplot as plt
+import tree
+
+#How far can a center be away from the last center and be in the same set
+deviation = 15
+#TODO:: I do not like this
+maxNeighborDistance = 100
+#TODO:: I think this needs work
+# First over-estimated guess for cell neighbors
+upperCutoffDistance = 5000
+
+#Loads image
+imageDir = "SampleImages/"
+imageName = "spidergfpapril12_11_z01_t021.tif"
+imagePath = os.path.join(imageDir,imageName)
+cells,image = sI.segmentImage(imagePath)
+
+#This box is the default for the tree geometry
+box = tree.Rectangle(0,0,image.shape[0],image.shape[1])
+#Puts Cutoff length in format of tree cutoffThreshold
+upperCutoff = upperCutoffDistance/image.shape[1]
+#Creates Tree
+root = tree.treeNode(box,list(cells),upperCutoff)
+
+#Finds neighbors of cells using tree structure
+#TODO:: Have root passed. Will not work when I move this code out of tree.py
+tree.findCloseCells(root,cells,deviation,maxNeighborDistance)
+
+#This Draws the lines neighboring cells
+for cell in cells:
+    cv.circle(image, (cell["center"][0],cell["center"][1]), 3, (255, 255, 0), -1)
+    for neighbor in cell["neighbors"]:
+        cv.line(image,cell["center"],neighbor,(132,124,255), 2)
+
+#This converts the image to the same color format as pyplot.
+image = cv.cvtColor(image,cv.COLOR_BGR2RGB)
+#Displays image with all alterations applied
+plt.imshow(image)
+plt.show()
--- tree.py
+++ tree.py
@@ -1,14 +1,8 @@
 # This is a Barnes-Hut Tree. It might be used for neighborhood estimation if this makes it any faster.
 # It also might be used to compare the closeness to estimations across multiple images
 from math import dist
-from scipy import constants
 import numpy as np
-import matplotlib.pyplot as plt
-import os
-import cv2 as cv
-import segmentImage as sI
 import sorting as srt
-
 from anytree import NodeMixin, RenderTree, PreOrderIter
 from enum import Enum
 
@@ -168,16 +162,16 @@ class treeUpdateActions(Enum):
 
 
 # This finds the neighbors using the moments of the cells
-def findCloseCells(cells,deviation,maxNeighborDistance):
+def findCloseCells(root,cells,deviation,maxNeighborDistance):
     # Finds the distance between all cells and all other cells within initial cutoff distances from each other.
-    walkTree(cells,treeUpdateActions.DISTANCES)
+    walkTree(root,cells,treeUpdateActions.DISTANCES)
     # Finds the largest distance around each cell from probable neighbors.
-    walkTree(cells,treeUpdateActions.MAX_DISTANCE,deviation)
+    walkTree(root,cells,treeUpdateActions.MAX_DISTANCE,deviation)
     # Finds probable neighbors for each cell.
-    walkTree(cells,treeUpdateActions.NEIGHBORS,maxNeighborDistance=maxNeighborDistance)
+    walkTree(root,cells,treeUpdateActions.NEIGHBORS,maxNeighborDistance=maxNeighborDistance)
 
 # This correctly navigates the tree structure. Uses nodes that need to be used, and ignores decendants of those that don't
-def walkTree(cells,updateAction,deviation=0,maxNeighborDistance=0):
+def walkTree(root,cells,updateAction,deviation=0,maxNeighborDistance=0):
     for cell in cells:
         nodeIterator = PreOrderIter(root)
         # Start looking through all nodes. Skip child nodes is parent node is past the cutoff
@@ -198,46 +192,3 @@ def treeActions(node,cell,deviation,maxNeighborDistance,updateAction):
         node.findMaxNeighborDistance(deviation)
     elif (updateAction==treeUpdateActions.NEIGHBORS):
         node.findNeighbors(cell,maxNeighborDistance)
-
-
-
-
-
-
-
-#How far can a center be away from the last center and be in the same set
-deviation = 15
-#TODO:: I do not like this
-maxNeighborDistance = 100
-#TODO:: I think this needs work
-# First over-estimated guess for cell neighbors
-upperCutoffDistance = 5000
-
-#Loads image
-imageDir = "SampleImages/"
-imageName = "spidergfpapril12_11_z01_t021.tif"
-imagePath = os.path.join(imageDir,imageName)
-cells,image = sI.segmentImage(imagePath)
-
-#This box is the default for the tree geometry
-box = Rectangle(0,0,image.shape[0],image.shape[1])
-#Puts Cutoff length in format of tree cutoffThreshold
-upperCutoff = upperCutoffDistance/image.shape[1]
-#Creates Tree
-root = treeNode(box,list(cells),upperCutoff)
-
-#Finds neighbors of cells using tree structure
-#TODO:: Have root passed. Will not work when I move this code out of tree.py
-findCloseCells(cells,deviation,maxNeighborDistance)
-
-#This Draws the lines neighboring cells
-for cell in cells:
-    cv.circle(image, (cell["center"][0],cell["center"][1]), 3, (255, 255, 0), -1)
-    for neighbor in cell["neighbors"]:
-        cv.line(image,cell["center"],neighbor,(132,124,255), 2)
-
-#This converts the image to the same color format as pyplot.
-image = cv.cvtColor(image,cv.COLOR_BGR2RGB)
-#Displays image with all alterations applied
-plt.imshow(image)
-plt.show()
