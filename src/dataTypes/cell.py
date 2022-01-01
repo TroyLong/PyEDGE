e1eb7b4859f8a080885e6f23d93109e5f989ed9f
TroyLong <tlong1299@gmail.com>
2022-01-01 17:59:34 -0600

    cellDistance function now available

--- src/dataTypes/cell.py
+++ src/dataTypes/cell.py
@@ -1,10 +1,9 @@
-#TODO:: Neighborguesses is just taking up extra memory.
-#TODO:: Neighbors might need to be its own datatype.
-
 ########################
 ## Imported Libraries ##
 ########################
 from enum import Enum, auto
+from math import dist
+
 
 # The enum is for type completion for my cell dictionary's keys.
 class cellTraits(Enum):
@@ -15,6 +14,11 @@ class cellTraits(Enum):
     NEIGHBORS = auto()
 
 # A dictionary is used over a traditional object for speed
-# TODO:: I'm not sure if I should use a tuple or a list for neighbors seeing as how often it will be changed
+# TODO:: I'm not sure if I should use a tuple or a list for
+# neighbors seeing as how often it will be changed
 # TODO:: I need to change verticies to tuple
-cell = {cellTraits.CENTER:(0,0), cellTraits.VERTICIES:list(), cellTraits.AREA:0, cellTraits.RADIUS:0, cellTraits.NEIGHBORS:tuple()}
+cell = {cellTraits.CENTER:(0,0), cellTraits.VERTICIES:list(),
+        cellTraits.AREA:0, cellTraits.RADIUS:0, cellTraits.NEIGHBORS:tuple()}
+
+def cellDist(cell1, cell2):
+    return dist(cell1[cellTraits.CENTER],cell2[cellTraits.CENTER])
