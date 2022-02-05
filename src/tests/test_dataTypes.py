ed2976503f81bc1a4b626a36550c22216e91a687
TroyLong <tlong1299@gmail.com>
2022-02-05 00:44:32 -0600

    started testing suite

---
+++ src/tests/test_dataTypes.py
@@ -0,0 +1,27 @@
+from copy import copy
+import dataTypes.cell as ce
+import dataTypes.cellNeighbor as cN
+import dataTypes.imageState as iS
+from dataTypes.dataTypeTraits import cellTraits as cT
+from dataTypes.dataTypeTraits import cellNeighborTraits as cNT
+from dataTypes.dataTypeTraits import imageStateTraits as iST
+
+mockCell1 = copy(ce.cell)
+mockCell2 = copy(ce.cell)
+mockCell3 = copy(ce.cell)
+
+mockCellNeighbor1 = copy(cN.cellNeighbor)
+mockCellNeighbor2 = copy(cN.cellNeighbor)
+mockCellNeighbor3 = copy(cN.cellNeighbor)
+
+
+def test_cell():
+    pass
+
+
+def test_cellNeighbor():
+    pass
+
+
+def test_imageState():
+    pass
